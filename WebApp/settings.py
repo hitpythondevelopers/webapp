c61f6cb8ddca746d73b8519f69c24233506514aa
tsitsi-flora <tsitsimunikwa97@gmail.com>
2019-02-14 10:27:50 +0200

    changes meant for hosting

--- WebApp/settings.py
+++ WebApp/settings.py
@@ -37,8 +37,6 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
-    'django_registration',
-    'crispy_forms',
     'website',
     'member',
     'blog',
