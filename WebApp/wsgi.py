10dd3170c011d7cc5efd2127bdd30c22d4cf5f89
tsitsi-flora <tsitsimunikwa97@gmail.com>
2019-03-15 14:16:10 +0200

    oky what now

--- WebApp/wsgi.py
+++ WebApp/wsgi.py
@@ -8,9 +8,10 @@ https://docs.djangoproject.com/en/2.1/howto/deployment/wsgi/
 """
 
 import os
+import socket
 
 from django.core.wsgi import get_wsgi_application
 
 os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'WebApp.settings')
-
 application = get_wsgi_application()
+
